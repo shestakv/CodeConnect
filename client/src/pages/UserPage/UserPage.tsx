8ebd06301d62dc7294f605b0e6e4d7f68bc646f4
ArsenAvanesyan <01borzik@gmail.com>
2024-10-03 13:41:23 +0300

    feat: add style footer, user page

--- client/src/pages/UserPage/UserPage.tsx
+++ client/src/pages/UserPage/UserPage.tsx
@@ -1,5 +1,4 @@
 import { useAppSelector } from "@/shared/hooks/reduxHooks";
-import { stat } from "fs";
 import styles from "./UserPage.module.css";
 
 type UserPageProps = {}
@@ -10,10 +9,35 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
     
     return (
       <>
-        <div className={styles.titleHeader}>
-          <img src={`${import.meta.env.VITE_IMG}${user.avatar}`} />
-          {user && user?.firstname}
-          {user && user?.surname}
+        <div className={styles.container}>
+          <div className={styles.titleHeader}>
+            <img
+              className={styles.avatar}
+              src={`${import.meta.env.VITE_IMG}${user.avatar}`}
+            />
+            <div>
+              {user && user?.firstname} {user && user?.surname}{" "}
+              {user && user?.patronymic}
+            </div>
+          </div>
+          <div className={styles.workExperience}>
+            <h3 className={styles.title}>Опыт работы: </h3>{" "}
+            {user && user?.workExperience}
+          </div>
+          <div className={styles.education}>
+            <h3 className={styles.title}>Образование: </h3>{" "}
+            {user && user?.education}
+          </div>
+          <div className={styles.bio}>
+            <h3 className={styles.title}>Информация: </h3> {user && user?.bio}
+          </div>
+          <div className={styles.phone}>
+            <h3 className={styles.title}>Телефон: </h3> {user && Number(user?.phone)}
+          </div>
+          <div className={styles.location}>
+            <h3 className={styles.title}>Локация: </h3>
+            {user && user?.location}
+          </div>
         </div>
       </>
     );
