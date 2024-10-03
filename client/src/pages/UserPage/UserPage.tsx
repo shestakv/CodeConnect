d2bc2fdb3525539502931f0bf77f8a9105697612
ArsenAvanesyan <01borzik@gmail.com>
2024-10-03 16:05:07 +0300

    feat: add style user page container

--- client/src/pages/UserPage/UserPage.tsx
+++ client/src/pages/UserPage/UserPage.tsx
@@ -1,44 +1,80 @@
 import { useAppSelector } from "@/shared/hooks/reduxHooks";
 import styles from "./UserPage.module.css";
+import { SettingOutlined } from "@ant-design/icons";
 
-type UserPageProps = {}
+type UserPageProps = {};
 
 export const UserPage: React.FC<UserPageProps> = ({}) => {
-    const user = useAppSelector((state) => state.user.user);
-    console.log(user);
-    
-    return (
-      <>
-        <div className={styles.container}>
-          <div className={styles.titleHeader}>
+  const { user } = useAppSelector((state) => state.user);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.titleHeader}>
+          <div className={styles.avatarContainer}>
             <img
               className={styles.avatar}
-              src={`${import.meta.env.VITE_IMG}${user.avatar}`}
+              src={`${import.meta.env.VITE_IMG}${user?.avatar}`}
             />
-            <div>
-              {user && user?.firstname} {user && user?.surname}{" "}
-              {user && user?.patronymic}
+            <div className={styles.settingIconImg}>
+              <SettingOutlined />
             </div>
           </div>
-          <div className={styles.workExperience}>
-            <h3 className={styles.title}>Опыт работы: </h3>{" "}
-            {user && user?.workExperience}
-          </div>
-          <div className={styles.education}>
-            <h3 className={styles.title}>Образование: </h3>{" "}
-            {user && user?.education}
-          </div>
-          <div className={styles.bio}>
-            <h3 className={styles.title}>Информация: </h3> {user && user?.bio}
-          </div>
-          <div className={styles.phone}>
-            <h3 className={styles.title}>Телефон: </h3> {user && Number(user?.phone)}
-          </div>
-          <div className={styles.location}>
-            <h3 className={styles.title}>Локация: </h3>
-            {user && user?.location}
+          <div>
+            {user && user.surname} {user && user.firstname}{" "}
+            {user && user.patronymic}
           </div>
         </div>
-      </>
-    );
-}
+        <div className={styles.secondContainer}>
+          <div className={styles.workExperience}>
+            <div className={styles.divider}>
+              <h3 className={styles.title}>Опыт работы: </h3>
+              <h3 className={styles.secondTitle}>
+                {user && user?.workExperience}
+              </h3>
+            </div>
+            <SettingOutlined className={styles.settingIcon} />
+          </div>
+        </div>
+        <div className={styles.secondContainer}>
+          <div className={styles.education}>
+            <div className={styles.divider}>
+              <h3 className={styles.title}>Образование: </h3>
+              <h3 className={styles.secondTitle}>{user && user?.education}</h3>
+            </div>
+            <SettingOutlined className={styles.settingIcon} />
+          </div>
+        </div>
+        <div className={styles.secondContainer}>
+          <div className={styles.bio}>
+            <div className={styles.divider}>
+              <h3 className={styles.title}>О Себе: </h3>
+              <h3 className={styles.secondTitle}>{user && user?.bio}</h3>
+            </div>
+            <SettingOutlined className={styles.settingIcon} />
+          </div>
+        </div>
+        <div className={styles.secondContainer}>
+          <div className={styles.phone}>
+            <div className={styles.divider}>
+              <h3 className={styles.title}>Телефон: </h3>{" "}
+              <h3 className={styles.secondTitle}>
+                {user && Number(user?.phone)}
+              </h3>
+            </div>
+            <SettingOutlined className={styles.settingIcon} />
+          </div>
+        </div>
+        <div className={styles.secondContainer}>
+          <div className={styles.location}>
+            <div className={styles.divider}>
+              <h3 className={styles.title}>Локация: </h3>
+              <h3 className={styles.secondTitle}>{user && user?.location}</h3>
+            </div>
+            <SettingOutlined className={styles.settingIcon} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
