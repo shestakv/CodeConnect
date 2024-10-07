fef85a145f5bad9920af8065078f8a6a27656ff8
Анастасия Малютина <dima.urf@mail.ru>
2024-10-07 19:19:59 +0300

    feat: favorite company button

--- client/src/entities/favoriteCompany/api/undex.ts
+++ client/src/entities/favoriteCompany/api/undex.ts
@@ -1,16 +1,16 @@
 import { axiosInstance } from "@/shared/lib/axiosInstance";
-import { FavoriteCompany, FavoriteCompanyResponse } from "../model";
+import { FavoriteCompany } from "../model";
 
 
 export class FavoriteCompanyServices {
-    static async getFavoriteCompanies(): Promise<FavoriteCompanyResponse> {
+    static async getFavoriteCompanies(): Promise<FavoriteCompany[]> {
         const response = await axiosInstance.get(`/favoritescompany`);
         return response.data;
     }
 
-    static async addFavoriteCompany(userId: number, companyId: number): Promise<FavoriteCompany[]> {
+    static async addFavoriteCompany(userId: number, companyId: number): Promise<FavoriteCompany> {
         const response = await axiosInstance.post("/favoritescompany", { userId, companyId });
-        console.log(response);
+        // console.log(response);
         return response.data;
     }
 
