f1195e9bde9459510925868a8d69081bf13dd40e
Анастасия Малютина <dima.urf@mail.ru>
2024-10-08 13:45:14 +0300

    feat: favorite company button

--- client/src/entities/company/ui/CompanyItem.tsx
+++ client/src/entities/company/ui/CompanyItem.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { ROUTES } from "@/app/router/routes";
 import { useAppDispatch, useAppSelector } from "@/shared/hooks/reduxHooks";
 import { addFavoriteCompany, deleteFavoriteCompany } from "@/entities/favoriteCompany";
+import { StarFilled, StarOutlined } from "@ant-design/icons";
 
 
 type Props = {
@@ -14,9 +15,10 @@ type Props = {
 const CompanyItem: React.FC<Props> = ({ company }) => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
+    const { favoriteCompanies } = useAppSelector((state) => state.favoriteCompanies)
     const userId = useAppSelector((state) => state.user.user?.id);
-    const { loading, error } = useAppSelector((state) => state.favoriteCompanies)
-    
+    const { error } = useAppSelector((state) => state.favoriteCompanies)
+    const isFavorite = favoriteCompanies.some(favCompany => favCompany.companyId === company.id && favCompany.userId === userId);
 
 
     const handleCardClick = () => {
@@ -37,35 +39,42 @@ const CompanyItem: React.FC<Props> = ({ company }) => {
 
     return (
 
-            <div className="container">
-                <button 
-                    onClick={handleCardClick} 
-                    className={styles.companyCard} 
-                >
-                    <img 
-                        className="companyLogo" 
-                        src={`${import.meta.env.VITE_IMG}${company.logo}`} 
-                        alt={`${company.name} логотип`} 
-                        style={{ width: '100px', height: '100px', borderRadius: '50%', objectFit: 'cover' }}
-                    />
-                    <h3 className={styles.companyName}>{company.name}</h3>
-                </button>
-                <button 
-                    onClick={handleAddToFavorites} 
-                    disabled={loading || !userId}
-                    className={styles.addFavoriteButton}
-                >
-                    {loading ? 'Добавление...' : 'Добавить в избранное'}
-                </button>
-                <button 
-                    onClick={handleRemoveFromFavorites} 
-                    disabled={loading || !userId}
-                    className={styles.removeFavoriteButton}
-                >
-                    {loading ? 'Удаление...' : 'Удалить из избранного'}
-                </button>
-                {error && <p className={styles.error}>{error}</p>}
-            </div>
+<div className="container">
+    <button 
+        onClick={handleCardClick} 
+        className={`${styles.companyCard} ${styles.cardButton}`}
+    >
+        <img 
+            className="companyLogo" 
+            src={`${import.meta.env.VITE_IMG}${company.logo}`} 
+            alt={`${company.name} логотип`} 
+            style={{ width: '100px', height: '100px', borderRadius: '50%', objectFit: 'cover' }}
+        />
+        <h3 className={styles.companyName}>{company.name}</h3>
+        
+        
+        <span 
+            onClick={(e) => {
+                e.stopPropagation();
+                if (isFavorite) {
+                    handleRemoveFromFavorites();
+                } else {
+                    handleAddToFavorites();
+                }
+            }} 
+            className={`${styles.favoriteButton} ${styles.favoritePosition}`} // Добавляем новый класс
+            title={isFavorite ? "Удалить из избранного" : "Добавить в избранное"}
+        >
+            {isFavorite ? (
+                <StarFilled style={{ color: 'blue' }} />
+            ) : (
+                <StarOutlined style={{ color: 'grey' }} />
+            )}
+        </span>
+
+        {error && <p className={styles.error}>{error}</p>}
+    </button>
+</div>
         );
     };
 
