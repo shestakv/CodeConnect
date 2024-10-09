e9054f26014489575e50ac7c10cd53383522cb2c
ArsenAvanesyan <01borzik@gmail.com>
2024-10-09 15:51:01 +0300

    feat: add icon, css

--- client/src/widgets/Footer/Footer.tsx
+++ client/src/widgets/Footer/Footer.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import { Layout } from "antd";
 import style from "./Footer.module.css";
+import {
+  YoutubeOutlined,
+  XOutlined,
+  InstagramOutlined,
+  FacebookOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 
 const { Footer } = Layout;
 
 export const FooterComponent: React.FC = () => {
   return (
-    <Footer
-      className={style.footer}
-    >
-      <p> © {new Date().getFullYear()} CodeConnect</p>
-    </Footer>
+    <div className={style.container}>
+      <Footer className={style.footer}>
+        <p> © {new Date().getFullYear()} CodeConnect</p>
+        <div className={style.secondContainer}>
+          <div className={style.icons}>
+            <YoutubeOutlined />
+          </div>
+          <div className={style.icons}>
+            <XOutlined />
+          </div>
+          <div className={style.icons}>
+            <InstagramOutlined />
+          </div>
+          <div className={style.icons}>
+            <FacebookOutlined />
+          </div>
+          <div className={style.icons}>
+            <LinkedinOutlined />
+          </div>
+        </div>
+      </Footer>
+    </div>
   );
 };
