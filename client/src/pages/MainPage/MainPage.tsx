e5e998a548919a5d9ac48b19b13b1273a18910f2
ArsenAvanesyan <01borzik@gmail.com>
2024-10-09 14:51:59 +0300

    feat: create main page and css

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -16,35 +16,35 @@ export const MainPage: React.FC<MainPageProps> = ({}) => {
       navigate(`/users/${user.id}`);
     }
   }, [user]);
-  
+
   return (
     <div>
       <Flex className={style.flex}>
         <div className={style.mainContainer}>
           <>
-            <div className={style.container2}>
-              <div className={style.secondContainer2}>
-                <h1>
+            <div className={style.container}>
+              <div className={style.secondContainer}>
+                <h1 className={style.mainTitle}>
                   <span className={style.char2}>C</span>ode
                   <span className={style.char2}>C</span>onnect
                 </h1>
                 <img
-                  className={style.img2}
+                  className={style.img}
                   data-original="https://static.tildacdn.com/tild6431-3730-4636-a563-376632393766/Untitled.svg"
                   alt=""
                   src="https://static.tildacdn.com/tild6431-3730-4636-a563-376632393766/Untitled.svg"
                   style={{ backgroundColor: "#578cec" }}
                 ></img>
-                <h1 className={style.title2}>
-                  Добро пожаловать в <span className={style.char2}>C</span>ode
-                  <span className={style.char2}>C</span>onnect! <br />
+                <h1 className={style.title}>
+                  Добро пожаловать в <span className={style.char}>C</span>ode
+                  <span className={style.char}>C</span>onnect! <br />
                   <br />
                   <p>Ищете разработчиков</p> для своего проекта или новую
                   работу? Вы находитесь в нужном месте!
                   <br />
                   <br />
-                  <span className={style.char2}>C</span>ode
-                  <span className={style.char2}>C</span>onnect — это
+                  <span className={style.char}>C</span>ode
+                  <span className={style.char}>C</span>onnect — это
                   инновационная платформа, которая облегчает поиск
                   квалифицированных специалистов и помогает разработчикам
                   находить интересные вакансии на основе их опыта и стека
@@ -52,10 +52,13 @@ export const MainPage: React.FC<MainPageProps> = ({}) => {
                   <br />
                   <br /> Начните свой путь к новым карьерным вершинам и находите
                   идеальных разработчиков с{" "}
-                  <span className={style.char2}>C</span>ode
-                  <span className={style.char2}>C</span>onnect! Присоединяйтесь
-                  к нашей платформе и реализуйте свои идеи или найдите работу
+                  <span className={style.char}>C</span>ode
+                  <span className={style.char}>C</span>onnect! Присоединяйтесь к
+                  нашей платформе и реализуйте свои идеи или найдите работу
                   своей мечты!
+                  <br />
+                  <br />
+                  <div>Для доступа ко всем возможностям сервиса, необходимо <Link to={"/signin"}> войти </Link> или <Link to={"/signup"}> зарегистрироваться</Link>.</div>
                 </h1>
               </div>
             </div>
