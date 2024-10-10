e6ea8b19964cf3376f820f6db21f804ac687cc84
shestakv <shevatriker@gmail.com>
2024-10-10 15:45:39 +0300

    Release2

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,20 @@ const serverConfig = require("./config/serverConfig");
 // Импортируем роуты из отдельных файлов
 const indexRouter = require("./routes/index.routes");
 const PORT = process.env.PORT ?? 3000;
-
+const path = require("path");
 
 const app = express(); // Создаем экземпляр приложения
+
+const staticFolder = path.join(__dirname, "..", "public", "dist");
 
 //конфигурация
 serverConfig(app); // запускаем конфигурацию (мидлварки)
 
 //мaршрутизация
 app.use("/api", indexRouter); // протягиваем сюда роуты, не прописывавем их здесь
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(staticFolder, "index.html"));
+});
 
 app.listen(PORT, () => console.log(`listen port ${PORT}`)); // запускаем порт
